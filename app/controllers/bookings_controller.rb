2b67510f11ec3ca50b7456dad93e9cdcbabd83f8
Ryu Watanabe <rw.ryunosuke@gmail.com>
2021-02-23 06:34:37 -0800

    bookings controller created, authorized tweaked, bookings view folder created

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,41 @@
 class BookingsController < ApplicationController
+
+	def index
+		@bookings = policy_scope(Booking)
+	end
+
+	def new
+		@car = Car.find(params[:car_id])
+		@booking = authorize Booking.new
+	end
+
+	def create
+		@booking = Booking.new(booking_params)
+		@booking.user = current_user
+		@booking.car = @car
+		authorize @booking
+		raise
+		if @booking.save
+			redirect_to bookings_path
+		else
+			render :new
+		end
+	end
+
+	def update
+	end
+
+	def edit
+	end
+
+	def destroy
+	end
+
+	def booking_params
+		params.require(:booking).permit(:start_date, :end_date, :user_id, :car_id)
+	end
+
+	def user_params
+	end
+
 end
