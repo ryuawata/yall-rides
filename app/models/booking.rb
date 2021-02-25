2b4504414837eff6240dcbc70fcdf9773842bcfe
Ryu Watanabe <rw.ryunosuke@gmail.com>
2021-02-24 19:46:27 -0800

    Status for bookings added, dummy users created in seeds

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
+	enum status: [:pending, :confirmed, :rejected]
   belongs_to :user
   belongs_to :car
   validates :start_date, presence: true 
   validates :end_date, presence: true
-  enum status: [ :pending, :confirmed, :rejected ]
 end
