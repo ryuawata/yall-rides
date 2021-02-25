39459816b2e1373f5f325a8118ccc34683eaf4fb
Anthony Nicholson <anthony.m.nicholson@gmail.com>
2021-02-25 14:25:11 +0900

    added search functionality on landing page for model, make and location

--- app/models/car.rb
+++ app/models/car.rb
@@ -10,4 +10,11 @@ class Car < ApplicationRecord
   has_one_attached :photo
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
+
+  include PgSearch::Model
+  pg_search_scope :search_by_car_details,
+    against: [ :model, :make, :location ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
