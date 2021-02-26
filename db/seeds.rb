5536688f408e86e90843fd9013dbbfb09e059ba3
Ryu Watanabe <rw.ryunosuke@gmail.com>
2021-02-25 23:32:21 -0800

    Added Japan Locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,55 +15,57 @@ User.destroy_all
 puts "Renters registering their cars..."
 
 user = User.create(
-	first_name: "Harry",
-	last_name: "Potter",
-	email: "rentee@yall.com",
-	password: "password"
-	)
-	
+  first_name: "Harry",
+  last_name: "Potter",
+  email: "rentee@yall.com",
+  password: "password"
+)
+
 file = URI.open('https://i.ytimg.com/vi/gY_i1LcDObU/maxresdefault.jpg')
 user.photo.attach(io: file, filename: 'user.jpg', content_type: 'image/jpg')
 
 user = User.create(
-	first_name: "Arthur",
-	last_name: "Weasley",
-	email: "renter@yall.com",
-	password: "password"
-	)
+  first_name: "Arthur",
+  last_name: "Weasley",
+  email: "renter@yall.com",
+  password: "password"
+)
 
 file = URI.open('https://static.wikia.nocookie.net/heroes-and-villain/images/5/5a/Arthur-weasley.jpg/revision/latest?cb=20181021155119')
 user.photo.attach(io: file, filename: 'user.jpg', content_type: 'image/jpg')
 
-10.times do
-	user = User.create!(
-		first_name: Faker::Movies::HarryPotter.character.split[0],
-		last_name: Faker::Movies::HarryPotter.character.split[1],
-		email: Faker::Internet.email,
-		password: Faker::Internet.password(min_length: 8)
-	)
+12.times do
+  user = User.create!(
+    first_name: Faker::Movies::HarryPotter.character.split[0],
+    last_name: Faker::Movies::HarryPotter.character.split[1],
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 8)
+  )
 
-	file = URI.open('https://thispersondoesnotexist.com/image')
-	user.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+  file = URI.open('https://thispersondoesnotexist.com/image')
+  user.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
 
-	fake_make = Faker::Vehicle.make.split.join
-	fake_model = Faker::Vehicle.model(make_of_model: fake_make).split.join
+  fake_make = Faker::Vehicle.make.split.join
+  fake_model = Faker::Vehicle.model(make_of_model: fake_make).split.join
 
-	puts "Fetching photo for #{fake_make} #{fake_model}..."
+  puts "Fetching photo for #{fake_make} #{fake_model}..."
 
-	file = URI.open("http://loremflickr.com/600/400/#{fake_make}-#{fake_model}")
+  file = URI.open("http://loremflickr.com/600/400/#{fake_make}-#{fake_model}")
 
-	car = Car.new(
-		make: fake_make,
-		model: fake_model,
-		year: Faker::Vehicle.year,
-		description: "#{Faker::Vehicle.standard_specs[0]} with #{Faker::Vehicle.standard_specs[1]}",
-		location: Faker::Address.city,
-		price: rand(500..3000)
-	)
-	car.photo.attach(io: file, filename: 'car.jpg', content_type: 'image/jpg')
-	car.user = user
-	car.save!
-	# file.rewind
+  cities = ["Tokyo", "Chiba", "Kyoto", "Nara", "Osaka", "Yokohama", "Kashiwasaki", "Toyama", "Hiroshima", "Nagano", "Nagoya", "Shizuoka", "Hamamatsu", "Tsukuba", "Nikko"]
+
+  car = Car.new(
+    make: fake_make,
+    model: fake_model,
+    year: Faker::Vehicle.year,
+    description: "#{Faker::Vehicle.standard_specs[0]} with #{Faker::Vehicle.standard_specs[1]}",
+    location: cities.sample,
+    price: rand(500..3000)
+  )
+  car.photo.attach(io: file, filename: 'car.jpg', content_type: 'image/jpg')
+  car.user = user
+  car.save!
+  # file.rewind
 end
 
 puts "All cars registered"
